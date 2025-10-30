8d208e434649bbd2216cc1dc86f61b7ccf6d3115
imran8102 <emranbagheri81@gmail.com>
2025-10-30 12:07:45 +0330

    feat: these sections are completed => 'popular courses section and recent courses section  (the carousel isn't  yet implemented),services and articles sections,home description section.'

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,10 @@ let mobileNav = document.querySelector('.mobile-nav')
 let overlay = document.querySelector('.overlay')
 let chevronDownIcons = document.querySelectorAll('.chevron-down-icon')
 let changeThemeBtn = document.querySelector('#theme')
+let hd_Btn = document.querySelector('#hd-btn')
+let hd_ContentElem = document.querySelector('#hd-content')
+let hd_Overlay = document.querySelector('#hd-overlay')
+
 
 //Opening the navbar by clicking on the navIcon
 navbarIcon.addEventListener('click', () => {
@@ -39,5 +43,24 @@ changeThemeBtn.addEventListener('click',(e) => {
     }else{
         themeBtn.setAttribute("xlink:href",'#moon')
         e.target.nextElementSibling.innerHTML = "تم تیره"
+    }
+})
+
+//view and hide content
+hd_Btn.addEventListener('click',(e) => {
+    console.log(e)
+    console.log(hd_Btn.innerHTML)
+    if(hd_Btn.innerHTML === 'مشاهده بیشتر'){
+        hd_Btn.innerHTML = 'مشاهده کمتر'
+        hd_Btn.classList.add('mt-8')
+        hd_ContentElem.classList.remove('hidden')
+        hd_ContentElem.parentElement.classList.remove('max-h-64')
+        hd_Overlay.classList.add('hidden')
+    }else{
+        hd_Btn.innerHTML = 'مشاهده بیشتر'
+        hd_Btn.classList.remove('mt-8')
+        hd_ContentElem.classList.add('hidden')
+        hd_ContentElem.parentElement.classList.add('max-h-64')
+        hd_Overlay.classList.remove('hidden')
     }
 })
